fca305e19b68ac3bfa530170995c73c7caf8878f
alexbejann <alex_bejan11@yahoo.com>
2023-03-06 21:27:45 +0100

    Finished name screen

--- lib/givt/impact_goal/quiz/pages/enter_username_page_2.dart
+++ lib/givt/impact_goal/quiz/pages/enter_username_page_2.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:givt_app_kids_game/givt/impact_goal/quiz/intro_name_final.dart';
 
-import 'package:givt_app_kids_game/givt/impact_goal/spinner/spinning_wheel_screen.dart';
-
-class EnterUsernameScreen extends StatefulWidget {
-  const EnterUsernameScreen({super.key});
+class EnterUsernameGalaxyScreen extends StatefulWidget {
+  const EnterUsernameGalaxyScreen({super.key});
 
   static Route<void> route() {
     return MaterialPageRoute<void>(
-      builder: (_) => const EnterUsernameScreen(),
+      builder: (_) => const EnterUsernameGalaxyScreen(),
     );
   }
 
   @override
-  State<EnterUsernameScreen> createState() => _EnterUsernameScreenState();
+  State<EnterUsernameGalaxyScreen> createState() =>
+      _EnterUsernameGalaxyScreenState();
 }
 
-class _EnterUsernameScreenState extends State<EnterUsernameScreen> {
+class _EnterUsernameGalaxyScreenState extends State<EnterUsernameGalaxyScreen> {
   final TextEditingController _controller = TextEditingController();
 
   String _name = '';
@@ -25,91 +25,109 @@ class _EnterUsernameScreenState extends State<EnterUsernameScreen> {
     if (_name.isEmpty) {
       return;
     }
-    Navigator.of(context).push<void>(SpinningWheelScreen.route());
+    Navigator.of(context).push<void>(
+      IntroNameFinal.route(
+        name: _name,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      backgroundColor: const Color(0xFFFFF9F3),
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(
-            horizontal: mediaQuery.size.width * 0.25,
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/images/solar_bg.svg',
+            fit: BoxFit.fill,
           ),
-          child: Column(
-            children: [
-              const Spacer(
-                flex: 3,
+          SafeArea(
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(
+                horizontal: mediaQuery.size.width * 0.25,
               ),
-              SvgPicture.asset(
-                'assets/images/givy_name.svg',
-              ),
-              const Spacer(
-                flex: 3,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: const Color(0xFF0A3C5F),
-                    width: 3,
+              child: Column(
+                children: [
+                  const Spacer(
+                    flex: 3,
                   ),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                child: TextField(
-                  controller: _controller,
-                  onChanged: (value) {
-                    setState(() {
-                      _name = value.trim();
-                    });
-                  },
-                  style: const TextStyle(
-                    fontSize: 30,
-                    color: Color(0xFF3B3240),
+                  Image.asset(
+                    'assets/images/givy_name_galaxy.png',
                   ),
-                  decoration: const InputDecoration(border: InputBorder.none),
-                  onSubmitted: (_) => onNameEntered(context),
-                ),
-              ),
-              const Spacer(),
-              ElevatedButton(
-                onPressed: _name.isEmpty
-                    ? null
-                    : () {
-                        onNameEntered(context);
-                      },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF0A3C5F),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+                  const Spacer(
+                    flex: 3,
                   ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 23),
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Next',
-                    style: TextStyle(
-                      fontSize: 30,
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 3,
+                      ),
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                    child: TextField(
+                      controller: _controller,
+                      onChanged: (value) {
+                        setState(() {
+                          _name = value.trim();
+                        });
+                      },
+                      style: const TextStyle(
+                        fontSize: 30,
+                        color: Color(0xFF3B3240),
+                      ),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Name',
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      onSubmitted: (_) => onNameEntered(context),
                     ),
                   ),
-                ),
+                  const Spacer(),
+                  ElevatedButton(
+                    onPressed: _name.isEmpty
+                        ? null
+                        : () {
+                            onNameEntered(context);
+                          },
+                    style: ElevatedButton.styleFrom(
+                      disabledBackgroundColor: const Color(0xFFBCBCBC),
+                      backgroundColor: const Color(0xFFFFB059),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 23),
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Next',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 5,
+                  ),
+                ],
               ),
-              const Spacer(
-                flex: 5,
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
