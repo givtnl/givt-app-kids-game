20e9a2eb81e75dbca7984e20ee1220d31f1c93fe
alexbejann <alex_bejan11@yahoo.com>
2023-03-07 16:39:04 +0100

    KIDS-51 Changed the givy bubble into a stateful widget

--- lib/givt/impact_goal/quiz/widgets/givy_bubble.dart
+++ lib/givt/impact_goal/quiz/widgets/givy_bubble.dart
@@ -2,13 +2,39 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:givt_app_kids_game/givt/impact_goal/quiz/widgets/chat_bubble.dart';
 
-class GivyBubble extends StatelessWidget {
+class GivyBubble extends StatefulWidget {
   const GivyBubble({
     required this.texts,
     super.key,
   });
 
   final List<String> texts;
+
+  @override
+  State<GivyBubble> createState() => _GivyBubbleState();
+}
+
+class _GivyBubbleState extends State<GivyBubble> {
+  String currentText = '';
+  int currentIndex = 0;
+  int textsIndex = 0;
+
+  void onNextText() {
+    if (currentIndex == textsIndex - 1) {
+      return;
+    }
+    currentIndex++;
+    setState(() {
+      currentText = widget.texts[currentIndex];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    currentText = widget.texts.first;
+    textsIndex = widget.texts.length;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +50,10 @@ class GivyBubble extends StatelessWidget {
           children: [
             ChatBubble(
               text: AnimatedTextKit(
+                isRepeatingAnimation: false,
+                onTap: onNextText,
                 animatedTexts: [
-                  TypewriterAnimatedText(
-                    texts.first,
-                    speed: const Duration(milliseconds: 50),
-                    textStyle: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF0A3C5F),
-                    ),
-                  ),
+                  getAnimatedText(currentText),
                 ],
               ),
             ),
@@ -74,6 +94,18 @@ class GivyBubble extends StatelessWidget {
         //   ),
         // ),
       ],
+    );
+  }
+
+  TypewriterAnimatedText getAnimatedText(String text) {
+    return TypewriterAnimatedText(
+      text,
+      speed: const Duration(milliseconds: 50),
+      textStyle: const TextStyle(
+        fontSize: 26,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF0A3C5F),
+      ),
     );
   }
 }
