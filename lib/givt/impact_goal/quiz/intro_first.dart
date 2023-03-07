cd3d7c187647c27d5922c59f4afb7143fbcc2a0e
alexbejann <alex_bejan11@yahoo.com>
2023-03-07 12:27:50 +0100

    removed useless class

--- lib/givt/impact_goal/quiz/intro_first.dart
+++ lib/givt/impact_goal/quiz/intro_first.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:givt_app_kids_game/givt/impact_goal/quiz/intro_final.dart';
-import 'package:givt_app_kids_game/givt/impact_goal/quiz/intro_second.dart';
 import 'package:givt_app_kids_game/givt/impact_goal/quiz/widgets/givy_bubble.dart';
 
 class IntroFirst extends StatelessWidget {
